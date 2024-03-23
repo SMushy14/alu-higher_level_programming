93526de18b420d795804d49cef620302e891f63c
SharonCode14 <s.mushy1@alustudent.com>
2024-03-23 09:48:33 -0700

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,12 +2,11 @@
 """Fetches https://alu-intranet.hbtn.io/status."""
 import urllib.request
 
+url = "https://alu-intranet.hbtn.io/status"
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alu-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+with urllib.request.urlopen(url) as response:
+    html = response.read().decode('utf-8')
+
+print("Body response:")
+print("\t- type:", type(html))
+print("\t- content:", html)
