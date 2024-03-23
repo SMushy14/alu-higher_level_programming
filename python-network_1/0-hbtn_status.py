e62ce23b571bb42f004afd6726048e95aea2f5e5
SharonCode14 <s.mushy1@alustudent.com>
2024-03-23 10:13:12 -0700

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,35 +1,13 @@
 #!/usr/bin/python3
-"""Fetches status from a given URL."""
-
-import sys
+"""Fetches https://alu-intranet.hbtn.io/status."""
 import urllib.request
-import urllib.error
 
-def fetch_status(url):
-    """
-    Fetches the status from the given URL using urllib.
-
-    Args:
-        url (str): The URL to fetch the status from.
-
-    Returns:
-        str: The status retrieved from the URL.
-    """
-    try:
-        request = urllib.request.Request(url)
-        with urllib.request.urlopen(request) as response:
-            body = response.read()
-            return body.decode("utf-8")
-    except urllib.error.URLError as e:
-        return "Error: {}".format(e.reason)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-    else:
-        url = "https://alu-intranet.hbtn.io/status"
-
-    status = fetch_status(url)
-    print("Body response:")
-    print("\t- type: {}".format(type(status)))
-    print("\t- content: {}".format(status))
+    request = urllib.request.Request("https://alu-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
