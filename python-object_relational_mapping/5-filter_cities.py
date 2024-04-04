8e2ea71fdb838ac8424c0e6f496ae0ee7ddfc90c
SharonCode14 <s.mushy1@alustudent.com>
2024-04-04 12:16:15 -0700

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Displays all states from the states table of the database hbtn_0e_4_usa."""
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
