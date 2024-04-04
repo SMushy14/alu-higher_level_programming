fa0a794406ca67df052996cf396348bbf05c1e21
SharonCode14 <s.mushy1@alustudent.com>
2024-04-04 13:02:50 -0700

    pep8 task 12

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Changes the name of the State object with id = 2 to New Mexico in the database hbtn_0e_6_usa."""
+"""Changes the name of the State object in the database hbtn_0e_6_usa."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
