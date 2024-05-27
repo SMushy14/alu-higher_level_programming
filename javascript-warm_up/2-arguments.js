7c80a08b4edea830e3b440f5ca435d9edcf641ee
root <s.mushy1@alustudent.com>
2024-05-27 07:37:12 -0700

    task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
+function printMessage(numArgs) {
+  if (numArgs === 0) {
+    console.log('No argument');
+  } else if (numArgs === 1) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
+}
 
+printMessage(arguments.length);
