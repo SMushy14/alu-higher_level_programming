74f93e47ffa927517bfdb14753b5fa1435060abd
root <s.mushy1@alustudent.com>
2024-05-28 03:31:38 -0700

    made thes script clear

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,4 +2,8 @@
 const args = process.argv;
 if (args[2] !== undefined && args[3] !== undefined) {
   console.log(`${args[2]} is ${args[3]}`);
+} else if (args[2] !== undefined) {
+  console.log(`${args[2]} is undefined`);
+} else {
+  console.log('undefined is undefined');
 }
