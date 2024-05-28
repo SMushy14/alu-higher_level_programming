3660f7780ba7d54d33a55a4abc1f353d7ace4796
root <s.mushy1@alustudent.com>
2024-05-28 04:36:22 -0700

    try and error

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const args = process.argv;
 
-const number = parseInt(args[2], 10);
+const firstArg = args[2];
+const number = parseInt(firstArg, 10);
 
-if (args[2]) {
+if (!firstArg) {
   console.log('Not a number');
-} else if (isNaN(number)) {
+} else if (!isNaN(number)) {
   console.log(`My number: ${number}`);
 } else {
   console.log('Not a number');
