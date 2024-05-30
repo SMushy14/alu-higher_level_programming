59719155ef351aac2c2fb8c28b59a029ae45b375
SharonCode14 <s.mushy1@alustudent.com>
2024-05-30 05:33:13 -0700

    task8 completed

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+function add(a, b) {
+  return a + b;
+}
+
+const a = Number(args[0]);
+const b = Number(args[1]);
+
+if (!isNaN(a) && !isNaN(b)) {
+  console.log(add(a, b));
+} else {
+  console.log("NaN");
+}
+
